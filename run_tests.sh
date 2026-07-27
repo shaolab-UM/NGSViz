346c5cc0c0c44e348a520310e3c3787e4ec01da1
bencheye <benche1993@163.com>
2026-07-27 11:11:22 +0800

    chore: stop tracking local logs and test runner

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH=/opt/homebrew/bin:$PATH
-cd /Users/benche/Desktop/myProj/ngsPlot/NGSViz
-mvn --version
-mvn test -Dtest=BedDBProcessTest,QueryGenomeRangeTest --batch-mode -q
